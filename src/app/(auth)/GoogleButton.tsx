9a3a3b9de15d289bd48fb32d16084f300567a9a7
Henry <vividnight@protonmail.com>
2024-05-07 16:12:07 -0400

    feat: add loading indicator to google sign in/up, also sign out

--- src/app/(auth)/GoogleButton.tsx
+++ src/app/(auth)/GoogleButton.tsx
@@ -1,18 +1,34 @@
+import { LoadingSpinner } from '@/components';
 import { signIn } from 'next-auth/react';
 import Image from 'next/image';
+import { useState } from 'react';
 
 export function GoogleButton({ variant }: { variant: 'login' | 'signup' }) {
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async () => {
+    setLoading(true);
+    await signIn('google');
+    setLoading(false);
+  };
+
   return (
-    <button className="w-full" onClick={() => signIn('google')}>
+    <button className="w-full" onClick={handleSubmit}>
       <div className="flex items-center justify-center rounded-md border-[1px] border-gray-400 px-2 py-1.5">
-        <Image
-          src="/google.svg"
-          alt="Google logo"
-          width={20}
-          height={20}
-          className="mr-3"
-        />
-        <p>Sign {variant === 'login' ? 'in' : 'up'} with Google</p>
+        {loading ? (
+          <LoadingSpinner />
+        ) : (
+          <>
+            <Image
+              src="/google.svg"
+              alt="Google logo"
+              width={20}
+              height={20}
+              className="mr-3"
+            />
+            <p>Sign {variant === 'login' ? 'in' : 'up'} with Google</p>
+          </>
+        )}
       </div>
     </button>
   );
--- src/components/SignOutButton.tsx
+++ src/components/SignOutButton.tsx
@@ -2,21 +2,37 @@
 
 import { signOut } from 'next-auth/react';
 import Image from 'next/image';
+import { useState } from 'react';
+import { LoadingSpinner } from './LoadingSpinner';
 
 export function SignOutButton() {
+  const [loading, setLoading] = useState(false);
+
+  const handleSignOut = async () => {
+    setLoading(true);
+    await signOut();
+    setLoading(false);
+  };
+
   return (
     <button
       className="relative flex w-[150px] items-center justify-center rounded-md bg-slate-900 px-3 py-2 text-sm text-white shadow-md"
-      onClick={() => signOut()}
+      onClick={handleSignOut}
     >
-      <span>Sign Out</span>
-      <Image
-        src="/signout.svg"
-        alt="Exit icon"
-        width={18}
-        height={18}
-        className="absolute right-0 mr-2"
-      />
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        <>
+          <span>Sign Out</span>
+          <Image
+            src="/signout.svg"
+            alt="Exit icon"
+            width={18}
+            height={18}
+            className="absolute right-0 mr-2"
+          />
+        </>
+      )}
     </button>
   );
 }
